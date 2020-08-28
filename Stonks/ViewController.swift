cfeca6fa5f6b209bb12a6b1a86d431646fca9a42
vesnna <vesna.nikita@yandex.ru>
2020-08-28 21:47:23 +0300

    realised layout of minimal required interface based on stack-views, outlets

--- Stonks/ViewController.swift
+++ Stonks/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameLbl: UITextField!
+    @IBOutlet weak var symbolLbl: UITextField!
+    @IBOutlet weak var priceLbl: UITextField!
+    @IBOutlet weak var priceChangeLbl: UITextField!
+    @IBOutlet weak var networkActivityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var picker: UIPickerView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
