7c5a91b2a120805f2dff35947e3620a4ac49005f
vesnna <vesna.nikita@yandex.ru>
2020-08-30 12:43:29 +0300

    realised PickerView with downloading info about selected company

--- Stonks/ViewController.swift
+++ Stonks/ViewController.swift
@@ -11,6 +11,13 @@ import UIKit
 class ViewController: UIViewController {
     
     lazy var infoManager = APIStockManager(apiKey: "pk_4eae54de1f214380a20f3d0825b1dfb2")
+    var companies = [
+        "Apple": "AAPL",
+        "Microsoft": "MSFT",
+        "Google": "GOOG",
+        "Amazon": "AMZN",
+        "Facebook": "FB"
+    ]
 
     @IBOutlet weak var nameLbl: UITextField!
     @IBOutlet weak var symbolLbl: UITextField!
@@ -23,32 +30,52 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        picker.dataSource = self
+        picker.delegate = self
+        
         onStartup()
         
-        requestStockInfo(for: "AAPL")
+        
         
     }
     
     
     func onStartup() {
         networkActivityIndicator.hidesWhenStopped = true
-        networkActivityIndicator.color = .black
+        //networkActivityIndicator.color = .black
         
+        removeTextOnLoad()
+        
+        let selectedRow = picker.selectedRow(inComponent: 0)
+        let selectedSymbol = Array(companies.values)[selectedRow]
+        
+        requestStockInfo(for: selectedSymbol)
+    }
+    
+    func removeTextOnLoad() {
         let removeTextOnLoad = [nameLbl, symbolLbl, priceLbl, priceChangeLbl]
         for label in removeTextOnLoad {
             label?.text = "-"
+            label?.textColor = .black
         }
+    }
+    
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        let selectedSymbol = Array(companies.values)[row]
+        requestStockInfo(for: selectedSymbol)
     }
     
     func requestStockInfo(for symbol: String) {
         
+        networkActivityIndicator.startAnimating()
+        removeTextOnLoad()
         
-        DispatchQueue.global(qos: .userInitiated).sync {
-            networkActivityIndicator.startAnimating()
+        DispatchQueue.global(qos: .userInitiated).async {
             
-            infoManager.fetchStockInfoFor(symbol: "AAPL") { (result) in
-                
-                //self.toggleActivityIndicator(active: false)
+            
+            self.infoManager.fetchStockInfoFor(symbol: symbol) { (result) in
                 
                 switch result {
                 case .Success(let stockInfo):
@@ -63,11 +90,10 @@ class ViewController: UIViewController {
                 }
             }
             
-            networkActivityIndicator.stopAnimating()
             
         }
         
-        
+        networkActivityIndicator.stopAnimating()
     }
     
     func updateUIWith(info stock: Stock) {
@@ -76,7 +102,37 @@ class ViewController: UIViewController {
         priceLbl.text = String(stock.price)
         priceChangeLbl.text = String(stock.change)
         
+        switch stock.change {
+        case ..<0:
+            priceChangeLbl.textColor = .red
+        case 0:
+            priceChangeLbl.textColor = .black
+        default:
+            priceChangeLbl.textColor = .green
+        }
     }
 
 }
 
+
+// MARK: - PickerViewDataSource
+
+extension ViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        companies.keys.count
+    }
+    
+}
+
+
+// MARK: - PickerViewDelegate
+
+extension ViewController: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return Array(companies.keys)[row]
+    }
+}
